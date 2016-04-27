db56cbdd556178b3ebf9764883508f778a9c8054
asherp <apembroke@gmail.com>
2016-04-27 10:18:05 -0400

    added post_install script to fixup python examples

---
+++ constructors/kameleon/osx/post_install.sh
@@ -0,0 +1,9 @@
+echo "Adding " ${PREFIX}/bin/python " to each python script"
+
+for file in ${PREFIX}/bin/ccmc/examples/python/*.py; do sed -i '' '1i\
+#!'${PREFIX}'/bin/python \
+' $file; done
+
+echo "Making python scripts executable... "
+chmod a+x ${PREFIX}/bin/ccmc/examples/python/*.py
+
