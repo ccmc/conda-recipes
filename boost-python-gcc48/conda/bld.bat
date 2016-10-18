714d6f6947c32de4dbd32056e3e708375fd5851c
asherp <apembroke@gmail.com>
2016-10-18 15:33:08 -0400

    added boost-gcc48 for linux and corresponding kameleon build

---
+++ boost-python-gcc48/conda/bld.bat
@@ -0,0 +1,39 @@
+setlocal enableextensions
+
+if "%PY_VER%" == "3.4" (
+    set MSVC_VER="10.0"
+) else (
+    if "%PY_VER%" == "3.5" (
+        set MSVC_VER="14.0"
+    ) else (
+        set MSVC_VER="9.0"
+    )
+)
+
+set EXTRA_ARGS=""
+if %ARCH% EQU 64 (
+	set EXTRA_ARGS="address-model=64"
+)
+
+
+%PYTHON% -c "from __future__ import print_function; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc(True))" > temp.txt
+set /p PYTHON_INCLUDE_DIR=<temp.txt
+
+call bootstrap.bat
+
+if errorlevel 1 exit 1
+
+bjam.exe --debug-configuration ^
+         --user-config="%RECIPE_DIR%/user-config-win.jam" ^
+		 -sBZIP2_LIBPATH="%LIBRARY_LIB%" -sBZIP2_INCLUDE="%LIBRARY_INC%" -sBZIP2_BINARY=bzip2 ^
+		 -sZLIB_INCLUDE="%LIBRARY_INC%" -sZLIB_LIBPATH="%LIBRARY_LIB%" -sZLIB_BINARY=zlib ^
+		 link=shared toolset="msvc-%MSVC_VER%" %EXTRA_ARGS% stage
+
+if errorlevel 1 exit 1
+         
+robocopy "stage\lib" "%LIBRARY_BIN%" /E /NFL
+robocopy "boost" "%LIBRARY_INC%\boost" /E /NFL /NDL
+
+if errorlevel GTR 8 exit 1
+
+exit 0
---
+++ boost-python-gcc48/conda/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+MACOSX_DEPLOYMENT_TARGET=10.9
+INCLUDE_PATH=$PREFIX/include
+LIBRARY_PATH=$PREFIX/lib
+
+# Grab the include dir (borrowed from homebrew boost-python)
+export PYTHON_INCLUDE_DIR=`python -c "from __future__ import print_function; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc(True))"`
+
+# Seems that sometimes this is required
+chmod -R 777 .*
+
+# Setup the boost building, this is fairly simple.
+./bootstrap.sh --prefix="${PREFIX}" --libdir="${LIBRARY_PATH}"
+# We need a host of options to get things to work on OSX
+if [ `uname` == Darwin ]; then
+
+  # Boost is not happy about the 'm' on the end of the lib
+  # therefore wejust symlink it without the 'm' as suggested
+  # here:
+  # https://groups.google.com/a/continuum.io/forum/#!searchin/anaconda/boost/anaconda/bAHVcb27vwY/3De25q8Z648J
+  if [ $PY3K -eq 1 ]; then
+    CDIR=`pwd`
+    cd $LIBRARY_PATH
+    ln -s libpython${PY_VER}m.dylib libpython${PY_VER}.dylib
+    cd $CDIR
+  fi
+  
+  CXXFLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+  LINKFLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+
+  # Need to default to clang over g++ (though in later version g++ is clang)
+  B2ARGS="toolset=clang -a"
+  
+  # If we want C++11 support, we need to enable these flags
+  # AND change the version min >= 10.7
+  CXXFLAGS="${CXXFLAGS} -std=c++11 -stdlib=libc++"
+  LINKFLAGS="${LINKFLAGS} -stdlib=libc++"
+  
+  CXXFLAGS="cxxflags=${CXXFLAGS}"
+  LINKFLAGS="linkflags=${LINKFLAGS}"
+else
+  CXXFLAGS="-std=c++11" 
+  LINKFLAGS=""
+  B2ARGS="toolset=gcc"
+fi
+
+# So, we use a custom user-config.jam which is for setting the conda python options
+# We explicitly set to use conda's BZIP2 and ZLIB
+# We make sure that we build the appropriate ARCH and set the CXX flags appropriately
+./bjam --debug-configuration --user-config="${RECIPE_DIR}/user-config.jam" \
+                             -sBZIP2_LIBPATH="${LIBRARY_PATH}" -sBZIP2_INCLUDE="${INCLUDE_PATH}" \
+                             -sZLIB_LIBPATH="${LIBRARY_PATH}"  -sZLIB_INCLUDE="${INCLUDE_PATH}" \
+                             link=shared address-model=${ARCH} architecture=x86 \
+                             ${CXXFLAGS} ${LINKFLAGS} ${B2ARGS} stage
+
+# Copy the files across
+cp -a stage/lib "$PREFIX"
+cp -R boost "${INCLUDE_PATH}"
